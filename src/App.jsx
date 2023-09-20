be07e470efe0640e8e8751b063fce984b2743369
Amir <amirhkarimi888@gmail.com>
2023-09-20 22:26:27 +0330

    Blank project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div id="app">
+      <header>
+        <nav className="w-full"></nav>
+      </header>
+    </div>
+  )
+}
+
+export default App
